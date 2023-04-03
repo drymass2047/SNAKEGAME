16707e0e7024978721e05154c848b751d7ce84a0
drymass2047 <129131794+drymass2047@users.noreply.github.com>
2023-04-03 17:09:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("gameCanvas");
+ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const scoreElement = document.getElementById("score");
 
@@ -8,6 +8,7 @@ const foodColor = "#FF0000";
 let score = 0;
 let touchStartX = null;
 let touchStartY = null;
+let gameOver = false;
 
 function handleTouchStart(event) {
   touchStartX = event.touches[0].clientX;
@@ -66,8 +67,6 @@ function initGame() {
   gameLoop();
 }
 
-// The rest of the code remains the same
-
 class Snake {
   constructor() {
     this.positions = [{ x: canvas.width / 2, y: canvas.height / 2 }];
@@ -84,85 +83,96 @@ class Snake {
     this.positions.pop();
   }
 
-checkCollision(food) {
-  // Check if the snake has collided with the food
-  if (this.positions[0].x === food.position.x && this.positions[0].y === food.position.y) {
-    this.grow();
-    return true;
-  }
+  checkCollision(food) {
+    if (
+      this.positions[0].x === food.position.x &&
+      this.positions[0].y === food.position.y
+    ) {
+      this.grow();
+      return true;
+    }
 
-  // Check if the snake has collided with a wall
-  if (this.positions[0].x < 0 || this.positions[0].x >= canvas.width ||
-      this.positions[0].y < 0 || this.positions[0].y >= canvas.height) {
+    if (
+      this.positions[0].x < 0 ||
+      this.positions[0].x >=canvas.width ||
+this.positions[0].y < 0 ||
+this.positions[0].y >= canvas.height
+) {
+return false;
+}
+    for (let i = 1; i < this.positions.length; i++) {
+  if (
+    this.positions[0].x === this.positions[i].x &&
+    this.positions[0].y === this.positions[i].y
+  ) {
     return false;
   }
-
-  // Check if the snake has collided with itself
-  for (let i = 1; i < this.positions.length; i++) {
-    if (this.positions[0].x === this.positions[i].x && this.positions[0].y === this.positions[i].y) {
-      return false;
-    }
-  }
-
-  return true;
 }
 
-  grow() {
-    this.positions.push({ ...this.positions[this.positions.length - 1] });
-  }
-
-  draw() {
-    ctx.fillStyle = snakeColor;
-    this.positions.forEach((pos) => {
-      ctx.fillRect(pos.x, pos.y, grid, grid);
-    });
-  }
+return true;
 }
 
+grow() {
+this.positions.push({ ...this.positions[this.positions.length - 1] });
+}
 
-
+draw() {
+ctx.fillStyle = snakeColor;
+this.positions.forEach((pos) => {
+ctx.fillRect(pos.x, pos.y, grid, grid);
+});
+}
+}
 
 class Food {
-  constructor() {
-    this.position = { x: 0, y: 0 };
-    this.randomize();
-  }
-
-  randomize() {
-  this.position.x = Math.floor(Math.random() * ((canvas.width - grid) / grid)) * grid;
-  this.position.y = Math.floor(Math.random() * ((canvas.height - grid) / grid)) * grid;
+constructor() {
+this.position = { x: 0, y: 0 };
+this.randomize();
 }
 
-  draw() {
-    ctx.fillStyle = foodColor;
-    ctx.fillRect(this.position.x, this.position.y, grid, grid);
-  }
+randomize() {
+this.position.x = Math.floor(
+Math.random() * ((canvas.width - grid) / grid)
+) * grid;
+this.position.y = Math.floor(
+Math.random() * ((canvas.height - grid) / grid)
+) * grid;
 }
 
+draw() {
+ctx.fillStyle = foodColor;
+ctx.fillRect(this.position.x, this.position.y, grid, grid);
+}
+}
 
 const snake = new Snake();
 const food = new Food();
 
 function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+if (gameOver) {
+return;
+}
 
-  snake.move();
-  if (!snake.checkCollision(food)) {
-    gameIsRunning = false;
-    return;
-  }
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (snake.positions[0].x === food.position.x && snake.positions[0].y === food.position.y) {
-    food.randomize();
-    score++;
-  }
+snake.move();
 
-  snake.draw();
-  food.draw();
+if (!snake.checkCollision(food)) {
+gameOver = true;
+return;
+}
 
-  updateScore();
+if (snake.positions[0].x === food.position.x && snake.positions[0].y === food.position.y) {
+food.randomize();
+score++;
+}
 
-  setTimeout(gameLoop, 100);
+snake.draw();
+food.draw();
+
+updateScore();
+
+setTimeout(gameLoop, 100);
 }
 
 initGame();
